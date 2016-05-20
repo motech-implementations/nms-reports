4ecd71ec19f0ff061983ad7e25fa853b95e6fa92
frankhuster <fhuster@grameenfoundation.org>
2016-05-20 16:05:28 -0700

    ETL process now calculates call_duration using callAnswerTime
    Additionally:
    - Removed hard-coded database dependency from the operator_dimension job
    - Removed useless ETL/ dir & added more useful MAMK and KK job directories
    containing the generated java code & jars that should be used for the
    actual ETL job.

--- Reporting-ETL/MA_MK_ETL_JOB/MA_MK_ETL_JOB/MA_MK_ETL_JOB_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/mysql-connector-java-3.1.14-bin.jar:$ROOT_PATH/../lib/mysql-connector-java-5.1.30-bin.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/ma_mk_etl_job_0_1.jar:$ROOT_PATH/circle_dimension_0_1.jar:$ROOT_PATH/ma_mk_data_0_1.jar:$ROOT_PATH/frontlineworker_0_1.jar:$ROOT_PATH/ma_call_detail_0_1.jar:$ROOT_PATH/log_etl_table_info_0_1.jar:$ROOT_PATH/ma_course_completion_0_1.jar:$ROOT_PATH/mk_call_detail_0_1.jar: final_etl.ma_mk_etl_job_0_1.MA_MK_ETL_JOB --context=Default "$@" 
